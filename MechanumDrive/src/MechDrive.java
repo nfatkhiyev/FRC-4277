43bae05b692a7240c2ba1b32655770b25a0eb3de
Cynopolis <31545331+Cynopolis@users.noreply.github.com>
2017-09-01 11:00:17 -0500

    Update MechDrive.java
    Could you add more comments to clarify what's going on, and the purpose of some of these methods?

--- MechanumDrive/src/MechDrive.java
+++ MechanumDrive/src/MechDrive.java
@@ -28,22 +28,24 @@ public class MechDrive {
 		
 		double xVal = stick.getX();
 		double yVal = stick.getY();//experiment with multiplying the values
+		double zVal = stick.getTwist();
 		
-		if(Math.abs(stick.getTwist()) > 0.1){
-			FrontRight.set(stick.getTwist());
-			BackLeft.set(-stick.getTwist());
-			FrontLeft.set(-stick.getTwist());
-			BackRight.set(stick.getTwist());
-		}
-		else{
-			FrontRight.set(yVal - xVal);
-			BackLeft.set(yVal - xVal);
-			FrontLeft.set(yVal + xVal);
-			BackRight.set(yVal + xVal);
-		}
+		/*
+		* Instead of providing an if statement when the joystick is twisted
+		* I just mapped the twist value to a z-axis, and added that to your
+		* set calls.
+		* I also have a test program in python that makes it easy to tell 
+		*if the wheels are spinning the correct way, and i've double checked
+		*your numbers in there. They're A-okay.
+		*/
+		
+		FrontRight.set(yVal - xVal - zVal);
+		BackRight.set(yVal + xVal - zVal);
+		FrontLeft.set(yVal + xVal + zVal);
+		BackLeft.set(yVal - xVal + zVal);
 	}
 	
-	public void mechDirectionalDrive (Double angle, Double speed, Double durration) {
+	public void mechDirectionalDrive (Double angle, Double speed, Double durration) { // I honestly don't know what's going on here
 		//Sets motor timeouts
 		FrontRight.setExpiration(durration);
 		FrontLeft.setExpiration(durration);
@@ -61,13 +63,13 @@ public class MechDrive {
 		FrontLeft.set(yVal + xVal);
 		BackRight.set(yVal + xVal);
 	}
-	public void mechSpinRight (Double speed){
+	public void mechSpinRight (Double speed){ // Is this for automation?
 		FrontRight.set(speed);
 		BackLeft.set(-speed);
 		FrontLeft.set(-speed);
 		BackRight.set(speed);
 	}
-	public void mechSpinLeft (Double speed){
+	public void mechSpinLeft (Double speed){ // Is this for automation?
 		FrontRight.set(-speed);
 		BackLeft.set(speed);
 		FrontLeft.set(speed);
